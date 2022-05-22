94796a44017ee36fcd6d3e9fafc7df91fed507e0
Kaaliie <lucas.tristant@hotmail.com>
2022-05-22 20:22:20 +0200

    Initialisation

---
+++ Portfolio_2022/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Navigation from "./components/Navigation.vue";
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <Navigation></Navigation>
+  </main>
+
+  <footer></footer>
+</template>
+
+<style lang="scss">
+@import "./assets/_variables.scss";
+@import "./assets/_fonts.scss";
+@import "./assets/_placeholder.scss";
+@import "./assets/setup.scss";
+</style>
---
+++ Portfolio_2022/src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<script></script>
+
+<template>
+  <h1>Titre Principal</h1>
+  <h2>Titre H2</h2>
+  <h3>Titre H3</h3>
+  <h4>Titre H4</h4>
+  <h5>Titre H5</h5>
+  <h6>Titre H6</h6>
+  <p>
+    There are many variations of passages of Lorem Ipsum available, but the
+    <strong
+      >majority have suffered alteration in some form, by injected humour,
+      or</strong
+    >
+    randomised words which don't look even slightly believable. If you are going
+    <em>to use a passage of Lorem Ipsum, you need</em> to be sure there isn't
+    anything embarrassing hidden in the middle of text. All the Lorem Ipsum
+    generators on the Internet tend to
+    <span>repeat predefined chunks as necessary, making this the</span>
+    first true generator on the
+    <a href="#">Lien vers un autre endroit</a>Internet. It uses a dictionary of
+    over 200 Latin words, combined with a handful of model sentence structures,
+    to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is
+    therefore always free from repetition, injected humour, or
+    non-characteristic words etc.
+  </p>
+  <button><a href="#">Lien vers un autre endroit</a></button>
+  <a href="#">Lien vers un autre endroit</a>
+</template>
+
+<style lang="scss"></style>
